ca7bdd478acdd5c140b5b2870a3554f39e792394
wjy <junyao.wang@dataforhealth.cn>
2020-08-06 16:08:30 +0800

    3

--- ServerNew/ServerNew/Program.cs
+++ ServerNew/ServerNew/Program.cs
@@ -108,6 +108,6 @@ namespace Server
     public class GitTest
     {
         public string Key { get; set; }
-        public string Whyy { get; set; }
+        public string Whyyyyy { get; set; }
     }
 }
